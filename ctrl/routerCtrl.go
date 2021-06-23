b096b2a99da45d9fbac11abe7b6a1809a240367b
89trillion-longqiang <longqiang@89trillion.com>
2021-06-23 16:24:11 +0800

    Increase parameter verification

--- ctrl/routerCtrl.go
+++ ctrl/routerCtrl.go
@@ -7,6 +7,12 @@ import (
 
 func GetStage(c *gin.Context)  {
 	stage := c.Query("stage")
+	if len(stage) == 0{
+		c.JSON(200, gin.H{
+			"condition":"parameter error",
+		})
+		return
+	}
 	retMap,retInfo := handle.HandleGetStage(stage)
 	c.JSON(200, gin.H{
 		"condition":retMap["condition"],
@@ -18,6 +24,12 @@ func GetArmyAssembled(c *gin.Context){
 	rarity := c.Query("rarity")
 	stage  := c.Query("stage")
 	cvc := c.Query("cvc")
+	if len(rarity)==0 && len(stage)==0 && len(cvc)==0{
+		c.JSON(200, gin.H{
+			"condition":"parameter error",
+		})
+		return
+	}
 	dataRes ,condition:= handle.HandleGetArmyAssembled(rarity,stage,cvc,*handle.UMAP)
 	c.JSON(200, gin.H{
 		"condition":condition,
@@ -29,7 +41,12 @@ func GetArmyAssembled(c *gin.Context){
 func IdGetRarity(c *gin.Context){
 	id := c.Query("id")
 	ret := handle.HandleIdGetRarity(id)
-
+	if len(id) == 0 && len(ret) ==0 {
+		c.JSON(200, gin.H{
+			"condition":"parameter error",
+		})
+		return
+	}
 	c.JSON(200, gin.H{
 		"condition":ret["condition"],
 		"id":ret["id"],
@@ -38,6 +55,12 @@ func IdGetRarity(c *gin.Context){
 }
 func IdGetCombatPoints(c *gin.Context){
 	id := c.Query("id")
+	if len(id) == 0 {
+		c.JSON(200, gin.H{
+			"condition":"parameter error",
+		})
+		return
+	}
 	ret := handle.HandleIdGetCombatPoints(id)
 
 	c.JSON(200, gin.H{
@@ -50,8 +73,14 @@ func IdGetCombatPoints(c *gin.Context){
 
 func GetSoldiersJson(c *gin.Context){
 	stage := c.Query("stage")
-	retMap , retInfo := handle.HandleGetSoldiersJson(stage)
+	if len(stage) ==0 {
+		c.JSON(200, gin.H{
+			"condition":"parameter error",
+		})
+		return
+	}
 
+	retMap , retInfo := handle.HandleGetSoldiersJson(stage)
 	c.JSON(200, gin.H{
 		"condition":retMap["condition"],
 		"stage" : retMap["stage"],
